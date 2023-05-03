8e55c5f3a85044120b5c43257f0c36d88288180a
panchunming-ghq <panchunming-ghq@sinosig.com>
2023-05-03 23:31:17 +0800

    111

---
+++ netty-control-netty-base/src/main/java/c5/TestJdkFuture.java
@@ -0,0 +1,24 @@
+package c5;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+@Slf4j
+public class TestJdkFuture {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        
+        Future<Integer> future =  service.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                log.debug("执行计算");
+                Thread.sleep(1000);
+                return 50;
+            }
+        });
+        log.debug("等待结果");
+        log.debug("结果为：{}",future.get());
+    }
+}
